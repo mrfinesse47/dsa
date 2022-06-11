ae5bf9b53920ab1a84bffcf976dff4a8092ad108
Kevin Mason <krmason2020@gmail.com>
2022-06-10 19:58:08 -0700

    completed nested even sum

--- pain-and-misery-recursion/capitalize.js
+++ pain-and-misery-recursion/capitalize.js
@@ -2,14 +2,17 @@ function capitalizeFirst(arr) {
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-  // add whatever parameters you deem necessary - good luck!
-  let newArr = [];
+  let newArr = []; //a new arr is required as to not affect the mem loc of arr, its  basically a new pointer
   if (arr.length === 0) {
+    console.log("base case:", newArr);
     return newArr;
   }
 
   newArr.push(capitalizeFirstLetter(arr[0]));
-  return newArr.concat(capitalizeFirst(arr.slice(1)));
+  const res = newArr.concat(capitalizeFirst(arr.slice(1)));
+  console.log("newarr:", newArr);
+  console.log("res:", res);
+  return res;
 }
 
 console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
