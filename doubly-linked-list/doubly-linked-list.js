4eba49ea31d735ad5a24dc0249a10eefb5938897
Kevin Mason <krmason2020@gmail.com>
2022-06-18 13:24:58 -0700

    completed unshift

--- doubly-linked-list/doubly-linked-list.js
+++ doubly-linked-list/doubly-linked-list.js
@@ -26,31 +26,67 @@ class DoublyLinkedList {
     return this;
   }
   pop() {
+    if (this.length === 0) {
+      return;
+    }
+    if (this.length === 1) {
+      const node = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return node;
+    }
     const prev = this.tail.prev;
     const oldNode = this.tail;
+    oldNode.prev = null;
+    oldNode.next = null;
     this.tail = prev;
     prev.next = null;
     this.length--;
     return oldNode;
   }
+  shift() {
+    if (this.length === 0) {
+      return;
+    }
+    const oldNode = this.head;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+
+      if (this.length === 1) {
+        this.tail = this.head;
+      }
+    }
+
+    oldNode.next = null;
+
+    return oldNode;
+  }
   printAllVal() {
     let node = this.head;
     let index = 0;
+    if (this.length === 0) {
+      return;
+    }
     while (node.next) {
-      console.log(`${index}:`, node.val);
+      console.log(`index:${index}:`, node.val);
       node = node.next;
       index++;
     }
-    console.log(`${index}:`, node.val);
+    console.log(`index: ${index}:`, node.val);
     console.log("total length:", this.length);
   }
 }
 
 const list = new DoublyLinkedList();
-list.push(2);
-list.push(3);
-list.push(4);
+list.push(54);
 list.push(5);
-console.log(list.pop());
+list.push(4);
+
+console.log(list.shift());
 
 list.printAllVal();
