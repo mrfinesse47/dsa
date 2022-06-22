211f7949fbfe3812b2996d3538916d1186080dae
Kevin Mason <krmason2020@gmail.com>
2022-06-21 17:51:57 -0700

    frustration

---
+++ leet/sliding-window/stocks.js
@@ -0,0 +1,15 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+//Input: prices = [7,1,5,3,6,4]
+//Output: 6;
+
+function stocks(arr) {
+  let left = 0;
+  for (let right = 1; right < arr.length; right++) {}
+}
+
+stocks([7, 1, 5, 3, 6, 4]);
+//[5,250,7,1,5,3,6,4,9,5,3,2,8,1000]
+//we go for a new window looking for max each minimum
